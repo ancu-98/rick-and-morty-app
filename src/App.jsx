397f6c0235e89ff3d489d2523cb7b5ee62a39bcb
Ancu-98 <jdsebastianb30@hotmail.com>
2023-01-18 00:41:27 -0500

    adding slider and title

--- src/App.jsx
+++ src/App.jsx
@@ -51,9 +51,141 @@ function App() {
 
   return (
     <div className="App">
-      <article className='title_container'>
-        <h1 className='title'>Rick and Morty</h1>
-      </article>
+      <div className='title' >
+        <h1 className='border'>Rick and Morty</h1>
+        <h1 className='wave' >Rick and Morty</h1>
+      </div>
+
+      <div className='container_all'>
+        <div className='container_img'>
+          <input type="radio" id='1' name='img_slide' hidden />
+          <input type="radio" id='2' name='img_slide' hidden />
+          <input type="radio" id='3' name='img_slide' hidden />
+          <input type="radio" id='4' name='img_slide' hidden />
+          <input type="radio" id='5' name='img_slide' hidden />
+          <input type="radio" id='6' name='img_slide' hidden />
+          <input type="radio" id='7' name='img_slide' hidden />
+          <input type="radio" id='8' name='img_slide' hidden />
+          <input type="radio" id='9' name='img_slide' hidden />
+          <input type="radio" id='10' name='img_slide' hidden />
+          <input type="radio" id='11' name='img_slide' hidden />
+          <input type="radio" id='12' name='img_slide' hidden />
+          <input type="radio" id='13' name='img_slide' hidden />
+
+          <div className='slide'>
+
+            <div className='item_slide'>
+              <img src="/img/1.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/2.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/3.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/4.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/5.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/6.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/7.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/8.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/9.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/10.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/11.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/12.jpg" />
+            </div>
+
+            <div className='item_slide'>
+              <img src="/img/13.jpg" />
+            </div>
+
+          </div>
+
+          <div className='pagination'>
+
+            <label className='pagination_item' htmlFor="1">
+              <img src="/img/1.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="2">
+              <img src="/img/2.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="3">
+              <img src="/img/3.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="4">
+              <img src="/img/4.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="5">
+              <img src="/img/5.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="6">
+              <img src="/img/6.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="7">
+              <img src="/img/7.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="8">
+              <img src="/img/8.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="9">
+              <img src="/img/9.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="10">
+              <img src="/img/10.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="11">
+              <img src="/img/11.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="12">
+              <img src="/img/12.jpg" />
+            </label>
+
+            <label className='pagination_item' htmlFor="13">
+              <img src="/img/13.jpg" />
+            </label>
+
+          </div>
+        </div>
+
+      </div>
       <form className='form_container' onSubmit={handleSubmit}>
         <input
           id='idLocation'
@@ -73,7 +205,7 @@ function App() {
           :
           <>
             <LocationInfo location={location} />
-            <div className='card--container' >
+            <div className='card__container' >
               {
                 location?.residents.map(url => (
                   <CardResident
